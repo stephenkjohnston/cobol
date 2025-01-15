2e9957c12b256a97572e50cd42d9251bc6f86eb0
Stephen Johnston <stephenjohnston@protonmail.com>
2025-01-15 11:18:08 -0800

    Update comment style.

--- GNUCobol/Basic/hello_world.cbl
+++ GNUCobol/Basic/hello_world.cbl
@@ -1,10 +1,18 @@
-      * The IDENTIFICATION DIVISION provides metadata about our program,
-      * and is one of two mandatory DIVISIONS in COBOL. The PROGRAM-ID
-      * is the only required entry within this DIVISION.
+      *******************************************************************
+      *                   IDENTIFICATION DIVISION                       *
+      *******************************************************************
+      * The IDENTIFICATION DIVISION provides metadata about our program,*
+      * and is one of two mandatory DIVISIONS in COBOL. The PROGRAM-ID  *
+      * is the only required entry within this DIVISION.                *
+      *******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLOWORLDPROGRAM.
       
-      * The PROCEDURE DIVISION is where the program's logic is written.
+      *******************************************************************
+      *                     PROCEDURE DIVISION                          *
+      *******************************************************************
+      * The PROCEDURE DIVISION is where the program's logic is written. *
+      *******************************************************************
        PROCEDURE DIVISION.
 
       * The DISPLAY statement prints the contents to the output device,
